bf2c4f00d73c72daef1899876926d2291e3ce540
Girishu M S <girishums1999@gmail.com>
2022-12-19 19:26:15 +0530

    Added 2 more endpoints to feign client

--- src/main/java/com/at0m/common/feign/AvailableQuantityFeign.java
+++ src/main/java/com/at0m/common/feign/AvailableQuantityFeign.java
@@ -3,12 +3,20 @@ package com.at0m.common.feign;
 import com.at0m.common.model.AvailableQuantity;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @FeignClient(name="AT0M-AVAILABLE-QUANTITY-API")
 public interface AvailableQuantityFeign {
 
-    @GetMapping("/api/quantity")
-    List<AvailableQuantity> getAllQuantity();
+    @GetMapping("/api/quantities")
+    List<AvailableQuantity> getAllAvailableQuantities();
+
+    @PostMapping("/api/quantities")
+    List<AvailableQuantity> saveListOfQuantities(List<AvailableQuantity> availableQuantities);
+
+    @GetMapping("/api/quantity/{productName}")
+    AvailableQuantity getByProductname(@PathVariable String productName);
 }
